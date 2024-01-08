61a797160e486591fbde6aa6852bb81701816303
“red” <karatasmert255@gmail.com>
2024-01-08 21:52:32 +0300

    Fix formatting and update authSlice actions

--- src/service/useAuthCalls.jsx
+++ src/service/useAuthCalls.jsx
@@ -1,12 +1,19 @@
 import axios from "axios";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 import { useNavigate } from "react-router-dom";
-import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice";
-import { useDispatch } from "react-redux";
+import {
+  fetchFail,
+  fetchStart,
+  loginSuccess,
+  logoutSuccess,
+  registerSuccess,
+} from "../features/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const useAuthCalls = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
 
   const login = async (userInfo) => {
     dispatch(fetchStart());
@@ -25,9 +32,34 @@ const useAuthCalls = () => {
     }
   };
 
-  const register = async () => {};
+  const register = async (userInfo) => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/users/`,
+        userInfo
+      );
+      dispatch(registerSuccess(data));
+      navigate("/stock");
+    } catch (error) {
+      dispatch(fetchFail());
+    }
+  };
 
-  const logout = async () => {};
+  const logout = async () => {
+    dispatch(fetchStart());
+    try {
+      await axios.get(`${process.env.REACT_APP_BASE_URL}/auth/logout`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      toastSuccessNotify("Çıkış işlemi başarili.");
+      dispatch(logoutSuccess());
+      // navigate("/")
+    } catch (error) {
+      dispatch(fetchFail());
+      toastErrorNotify("Çıkış işlemi başarisiz oldu.");
+    }
+  };
 
   return { login, register, logout };
 };
