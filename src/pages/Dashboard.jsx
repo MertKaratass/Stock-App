55531f1faa788a8dd9ecc25604221a88a9323762
“red” <karatasmert255@gmail.com>
2024-01-08 20:48:29 +0300

    Update authSlice, stockSlice, Dashboard, Login, and PrivateRouter files

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
+import React from "react"
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import CssBaseline from "@mui/material/CssBaseline"
 
-import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { useSelector } from "react-redux";
+import Toolbar from "@mui/material/Toolbar"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import { useSelector } from "react-redux"
 
 function Dashboard() {
-  const { user } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth)
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -23,7 +23,7 @@ function Dashboard() {
         </Toolbar>
       </AppBar>
     </Box>
-  );
+  )
 }
 
-export default Dashboard;
+export default Dashboard
