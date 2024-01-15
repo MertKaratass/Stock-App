19a06e3cd06f753cb0a3ef048e3721c9cf559de5
“red” <karatasmert255@gmail.com>
2024-01-15 21:52:13 +0300

    Refactor ProductModal and Products components

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -4,22 +4,21 @@ import TextField from "@mui/material/TextField";
 import Modal from "@mui/material/Modal";
 import { modalStyle } from "../styles/globalStyles";
 import useStockCalls from "../service/useStockCalls";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-export default function FirmModal({ open, handleClose, info, setInfo }) {
-  const { postStock, putStock } = useStockCalls();
+export default function ProductModal({ open, handleClose, info, setInfo }) {
+  const { postStock } = useStockCalls();
 
   const handleChange = (e) => {
     setInfo({ ...info, [e.target.name]: e.target.value });
   };
 
-  console.log(info);
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (info._id) {
-      putStock("firms", info);
-    } else {
-      postStock("firms", info);
-    }
+    postStock("products", info);
     handleClose();
   };
 
@@ -38,43 +37,28 @@ export default function FirmModal({ open, handleClose, info, setInfo }) {
             component="form"
             onSubmit={handleSubmit}
           >
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Age"
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+
             <TextField
-              label="Firm Name"
+              label="Product Name"
               name="name"
               id="name"
               type="text"
               variant="outlined"
               value={info.name}
-              onChange={handleChange}
-              required
-            />
-            <TextField
-              label="Phone"
-              name="phone"
-              id="phone"
-              type="tel"
-              variant="outlined"
-              value={info.phone}
-              onChange={handleChange}
-              required
-            />
-            <TextField
-              label="Address"
-              name="address"
-              id="address"
-              type="text"
-              variant="outlined"
-              value={info.address}
-              onChange={handleChange}
-              required
-            />
-            <TextField
-              label="Image"
-              name="image"
-              id="image"
-              type="url"
-              variant="outlined"
-              value={info.image}
               onChange={handleChange}
               required
             />
